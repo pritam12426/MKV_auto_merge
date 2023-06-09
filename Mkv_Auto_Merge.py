16a7343172616137e46cba97e2717a17daf4e266
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-06-09 10:44:00 +0530

    This is pre version

--- Mkv_Auto_Merge.py
+++ Mkv_Auto_Merge.py
@@ -1,4 +1,11 @@
-import os, yt_dlp, pymkv, sys
+print("Loading...")
+import os
+import yt_dlp
+import pymkv
+import sys
+from time import time, strftime, gmtime
+
+# TODO: add merging command in merge_all_file function. for pymkv
 
 
 def get_type_system() -> tuple:
@@ -12,7 +19,32 @@ def get_type_system() -> tuple:
 		exit("Unsupported system.")
 
 
-def find_folder(path) -> bool:
+def get_lan() -> str:
+	print(" Language ".center(20, "="))
+	print(f"{'Hindi':17}: h\n{'English':17}: e\n{'Other language':17}: o\n")
+
+	lan: str = input("Video language [h/e/o] ? ")
+	if lan.lower() == "h":
+		return "hin"
+
+	elif lan.lower() == "e":
+		return "eng"
+
+	else:
+		return "mul"
+
+
+def formate_name(text: str) -> str:
+	import re
+	new_text: str = text.rsplit(".")[0]
+	new_text: str = new_text.rsplit("[")[0]
+	new_text: str = re.sub("_+", " ", new_text)
+	new_text: str = re.sub(" +", "_", new_text)
+	new_text: str = new_text.replace("_-_", "-").lower()
+	return new_text.removesuffix("_")
+
+
+def find_folder(path: str) -> bool:
 	if os.path.exists(path):
 		if os.path.isdir(path):
 			return True
@@ -24,7 +56,7 @@ def find_folder(path) -> bool:
 		exit(f"'{path}' --> Is not a directory of your computer.")
 
 
-def load_json(link) -> dict():
+def load_json(link: str) -> dict:
 	ydl_opts = {}
 
 	with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -33,7 +65,7 @@ def load_json(link) -> dict():
 
 
 def find_videos(path) -> bool:
-	all_videos = [n for n in os.listdir(path) if n.endswith((".mkv", ".mp4"))]
+	all_videos = [n for n in os.listdir(path) if n.endswith((".mkv", ".mp4", "webm"))]
 	if len(all_videos) == 1:
 		return False
 
@@ -44,35 +76,55 @@ def find_videos(path) -> bool:
 		exit(f"There is no file video file in --> '{path}'")
 
 
-def is_chapters(json_data) -> list[str]:
-	if not json_data['chapter'] == None:
-		pass
+# def is_chapters(data: dict) -> list[str]:
+# 	if data.get('chapters') != None:
+# 		return data.get('chapters')
 
+
+# def add_meta_data(data: dict):
+# 	pass
+
+
+def merge_all_file(all_file: list[str], output_path: str, input_path: str, lan: str) -> None:
+	if find_folder(output_path):
+
+		for i in all_file:
+			if i.endswith((".mkv", ".mp4", "webm")):
+				name: str = i.rsplit(".")[0]
+
+				merge_task = pymkv.MKVFile(title=formate_name(name))
+
+				merge_task.add_track(pymkv.MKVTrack(i, track_id=0, track_name=formate_name(name), language=lan, default_track=True))
+				merge_task.add_track(pymkv.MKVTrack(i, track_id=1, track_name=formate_name(name), language=lan, default_track=True))
+
+				if name + ".jpg" in all_file:
+					merge_task.add_attachment(name + ".jpg")
+
+				if name + ".srt" in all_file:
+					merge_task.add_track(pymkv.MKVTrack(input_path + name + ".srt", track_name=formate_name(name), language="eng"))
+
+				merge_task.mux(output_path + formate_name(name)+".mp4")
+
+
+os.system(get_type_system()[0])
 
 input_path: str = input("Insert input folder path >> ") + get_type_system()[1]
 
 if find_folder(input_path):
-	os.chdir(input_path)
-	all_file: list[str] = os.listdir()
-	url: str = input("Insert Video url >> " or "")
-
 	if find_videos(input_path):
-		for i in all_file: #  If it is a playlist:
-			if i.endswith((".mkv", ".mp4")):
-				name: str = i.rsplit(".")[0]
-
-				if name + ".jpg" in all_file:
-					pass
-
-				if name + ".srt" in all_file:
-					pass
-
-				if url != "":
-					if load_json(url)['chapters'] != None:
-						print("Found chapters")
+		pass
 
 	else:
-		print("There is only 1 videos.")
+		os.chdir(input_path)
+	all_file: list[str] = os.listdir()
+	language = get_lan()
+	# url: str = input("Insert Video url >> " or "")
+	output_path = input("Insert the output path >> " or "") + get_type_system()[1]
+
+	init = time()
+	merge_all_file(all_file, output_path, input_path, language)
+	print(" DONE ".center(20, "="))
+	print(f"Total time >>> {strftime('%H:%M:%S', gmtime(time() - init))} is taken.")
+
 else:
 	exit(f"'{input_path}' --> Is not a folder.")
-
