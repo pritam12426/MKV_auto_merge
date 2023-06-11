9c69045bd90d182c91be810c10a15cdf744c3d6c
Pritam <84720825+pritam12426@users.noreply.github.com>
2023-06-11 10:42:16 +0530

    Under dev

--- mkv_auto_merge.py
+++ mkv_auto_merge.py
@@ -1,20 +1,15 @@
 print("Loading...")
 import os
-import yt_dlp
-import pymkv
 import sys
+import pymkv
 from time import time, strftime, gmtime
-
-# TODO: add merging command in merge_all_file function. for pymkv
 
 
 def get_type_system() -> tuple:
 	if sys.platform == "win":
 		return "cls", "\\"
-
 	elif sys.platform == "linux":
 		return "clear", "/"
-
 	else:
 		exit("Unsupported system.")
 
@@ -24,12 +19,11 @@ def get_lan() -> str:
 	print(f"{'Hindi':17}: h\n{'English':17}: e\n{'Other language':17}: o\n")
 
 	lan: str = input("Video language [h/e/o] ? ")
+
 	if lan.lower() == "h":
 		return "hin"
-
 	elif lan.lower() == "e":
 		return "eng"
-
 	else:
 		return "mul"
 
@@ -48,62 +42,103 @@ def find_folder(path: str) -> bool:
 	if os.path.exists(path):
 		if os.path.isdir(path):
 			return True
-
 		else:
 			return False
-
 	else:
 		exit(f"'{path}' --> Is not a directory of your computer.")
 
 
-def load_json(link: str) -> dict:
-	ydl_opts = {}
-
-	with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-		info = ydl.extract_info(link, download=False)
-		return ydl.sanitize_info(info)
-
-
 def find_videos(path) -> bool:
 	all_videos = [n for n in os.listdir(path) if n.endswith((".mkv", ".mp4", "webm"))]
+
 	if len(all_videos) == 1:
 		return False
-
 	elif len(all_videos) > 1:
 		return True
-
 	else:
 		exit(f"There is no file video file in --> '{path}'")
 
 
-# def is_chapters(data: dict) -> list[str]:
-# 	if data.get('chapters') != None:
-# 		return data.get('chapters')
+def convert_time_to_int(time_string: list[str]) -> int:
+	return int(time_string[0]) * 3600 + int(time_string[1]) * 60 + int(time_string[2])
 
 
-# def add_meta_data(data: dict):
-# 	pass
+def is_chapters_is_there(name: str) -> bool:
+	with open(name + ".txt", "rt") as f:
+		file_content: str = f.read()
+
+	for line in file_content.splitlines():
+		if ":" in line and line[0:2].isdigit() and line[2] == ":" and line[3].isdigit():
+			return True
+
+	return False
 
 
-def merge_all_file(all_file: list[str], output_path: str, input_path: str, lan: str) -> None:
+def write_chapters(name: str, ) -> str:
+	with open(name + ".txt", "r") as f:
+		file_content: str = f.read()
+
+	chapters: str = ""
+	numbers_of_chapters: int = 0
+
+	for line in file_content.splitlines():
+		if ":" in line and line[0:2].isdigit() and line[2] == ":" and line[3].isdigit():
+
+			start: str = line.split(" ", maxsplit=1)[0]
+			title: str = line.split(" ", maxsplit=1)[1].title()
+
+			start: list[str] = start.split(":")
+
+			if len(start) == 2:
+				start: list[str] = ['00'] + start
+
+			numbers_of_chapters += 1
+			chapters += f"""CHAPTER{numbers_of_chapters}={":".join(start)}.000\nCHAPTER{numbers_of_chapters}NAME={title}\n\n"""
+
+	with open(name + "_chapters.txt", "w") as f:
+		f.write(chapters)
+
+	return name + "_chapters.txt"
+
+
+def merge_all_content(all_file: list[str], output_path: str, input_path: str, lan: str) -> tuple:
 	if find_folder(output_path):
+		playlist = 0
+		single_video = ""
+		play_list_or_not = find_videos(input_path)
 
 		for i in all_file:
 			if i.endswith((".mkv", ".mp4", "webm")):
 				name: str = i.rsplit(".")[0]
 
-				merge_task = pymkv.MKVFile(title=formate_name(name))
+				if play_list_or_not:
+					print(f"Working on video {playlist}", end="\r")
+				else:
+					print(single_video)
+
+				merge_task = pymkv.MKVFile(title=formate_name(name).replace("_", " "))
 
 				merge_task.add_track(pymkv.MKVTrack(i, track_id=0, track_name=formate_name(name), language=lan, default_track=True))
 				merge_task.add_track(pymkv.MKVTrack(i, track_id=1, track_name=formate_name(name), language=lan, default_track=True))
 
+				single_video += f"'{i}' as a main video file.\n"
+
 				if name + ".jpg" in all_file:
 					merge_task.add_attachment(name + ".jpg")
+					single_video += f"* >>> cover album.\n"
 
 				if name + ".srt" in all_file:
 					merge_task.add_track(pymkv.MKVTrack(input_path + name + ".srt", track_name=formate_name(name), language="eng"))
+					single_video += f"* >>> subtitle file.\n"
+
+				if name + ".txt" in all_file:
+					if is_chapters_is_there(name):
+						single_video += f"* >>> chapters file.\n"
+						write_chapters(name)
+						merge_task.chapters(input_path + name + "_chapters.txt", lan)
 
 				merge_task.mux(output_path + formate_name(name)+".mp4")
+				playlist += 1
 
 
 os.system(get_type_system()[0])
@@ -111,20 +146,17 @@ os.system(get_type_system()[0])
 input_path: str = input("Insert input folder path >> ") + get_type_system()[1]
 
 if find_folder(input_path):
-	if find_videos(input_path):
+	if find_videos(input_path):  # If present directory is not playlist
 		pass
-
-	else:
+	else:  # If present directory is not a playlist
 		os.chdir(input_path)
-	all_file: list[str] = os.listdir()
-	language = get_lan()
-	# url: str = input("Insert Video url >> " or "")
-	output_path = input("Insert the output path >> " or "") + get_type_system()[1]
+		all_file: list[str] = os.listdir()
+		language = get_lan()
+		output_path = input("Insert the output path >> " or "") + get_type_system()[1]
 
-	init = time()
-	merge_all_file(all_file, output_path, input_path, language)
-	print(" DONE ".center(20, "="))
-	print(f"Total time >>> {strftime('%H:%M:%S', gmtime(time() - init))} is taken.")
-
+		init = time()
+		merge_all_content(all_file, output_path, input_path, language)
+		print(" DONE ".center(20, "="))
+		print(f"Total time >>> {strftime('%H:%M:%S', gmtime(time() - init))} is taken.")
 else:
 	exit(f"'{input_path}' --> Is not a folder.")
--- txt_book/Chapter_Txt.py
+++ txt_book/Chapter_Txt.py
@@ -1,10 +1,10 @@
 import os
 from rich.console import Console
 
-output_paht = input("Inset the output path > ")  # will take input for working directory from the user.
-os.chdir(output_paht)  # Will chagne the directory to output_path.
+output_path = input("Inset the output path > ")  # will take input for working directory from the user.
+os.chdir(output_path)  # Will change the directory to output_path.
 
-all_file = os.listdir()  # Will list all file or floer of working directory.
+all_file = os.listdir()  # Will list all file or folder of working directory.
 
 n = 0
 for i in all_file:  # will loop all file that is present in the directory.
@@ -13,28 +13,29 @@ for i in all_file:  # will loop all file that is present in the directory.
 		n += 1
 
 if n != 1:
-	'''If ther is not file that is endwith '.mp4' or multipel file 
-	in working directory. The program will automaticli exit.'''
+	'''If there is not file that is endswith '.mp4' or multiple file 
+	in working directory. The program will automatically exit.'''
 	Console().print(f"\nThere are '{n}' [color(13) bold]'.mp4'[/] file in [color(9)]'{os.getcwd()}'[/] :eyes: directory\n")
 	exit()
 
-open(f"{file_name}_Chater.txt", "w") # Will make sure that "file chater.txt" is empity.
+open(f"{file_name}_Chater.txt", "w")  # Will make sure that "file chater.txt" is empty.
 
 
 def write():
 
 	chapter = 1
-	while(True):
+	while True:
 		time = input(f"\nInset the time of chapter {chapter} > ")
-		if time == "": # If The value of tiem is empity then file will close. 
+		if time == "":  # If The value of time is empty then file will close.
 			os.system("clear")
-			Console().print(f'[color(2)]Totle chapters are {chapter} done[/]')
+			Console().print(f'[color(2)]total chapters are {chapter} done[/]')
 			exit()
 		else:
 			with open(f'{file_name} Chater.txt', 'a') as file:
 				name = input(f"Inset the name of chapter {chapter} > ")
-				file.write(str(f'CHAPTER{chapter}={time}.000\n')) # Will write the timeing of chapter.
-				file.write(str(f'CHAPTER{chapter}NAME={name.title()}\n')) # Will write the chagne name.
+				file.write(str(f'CHAPTER{chapter}={time}.000\n'))  # Will write the timeing of chapter.
+				file.write(str(f'CHAPTER{chapter}NAME={name.title()}\n'))  # Will write the change name.
 				chapter += 1
+
 
 write()
